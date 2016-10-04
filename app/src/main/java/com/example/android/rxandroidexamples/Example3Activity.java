3030392b634e7f9a529bc784592e1f0b87e1af9c
Milten <Milten198@gmail.com>
2016-10-04 17:59:13 +0200

    Example 3

--- app/src/main/java/com/example/android/rxandroidexamples/Example3Activity.java
+++ app/src/main/java/com/example/android/rxandroidexamples/Example3Activity.java
@@ -7,61 +7,64 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.List;
 import java.util.concurrent.Callable;
 
-import rx.Observable;
-import rx.Observer;
+import rx.Single;
+import rx.SingleSubscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
-public class Example2Activity extends AppCompatActivity {
+public class Example3Activity extends AppCompatActivity {
 
     private Subscription mTvShowSubscription;
     private RecyclerView mTvShowListView;
     private ProgressBar mProgressBar;
     private SimpleStringAdapter mSimpleStringAdapter;
     private RestClient mRestClient;
+    private TextView mErrorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mRestClient = new RestClient(this);
         configureLayout();
-        createObservable();
+        createSingle();
     }
 
-    private void createObservable() {
-        Observable<List<String>> tvShowObservable = Observable.fromCallable(new Callable<List<String>>() {
+    private void createSingle() {
+        Single<List<String>> tvShowSingle = Single.fromCallable(new Callable<List<String>>() {
             @Override
-            public List<String> call() {
-                return mRestClient.getFavoriteTvShows();
+            public List<String> call() throws Exception {
+                return mRestClient.getFavoriteTvShowsWithException();
             }
         });
 
-        mTvShowSubscription = tvShowObservable
+        mTvShowSubscription = tvShowSingle
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        new Observer<List<String>>() {
-                            @Override
-                            public void onCompleted() {
+                .subscribe(new SingleSubscriber<List<String>>() {
+                    @Override
+                    public void onSuccess(List<String> value) {
+                        displayTvShows(value);
+                    }
 
-                            }
-
-                            @Override
-                            public void onError(Throwable e) {
-
-                            }
-
-                            @Override
-                            public void onNext(List<String> tvShows) {
-                                displayTvShows(tvShows);
-                            }
-                        });
+                    @Override
+                    public void onError(Throwable error) {
+                        displayErrorMessage();
+                    }
+                });
     }
+
+
+    private void displayErrorMessage() {
+        mProgressBar.setVisibility(View.GONE);
+        mErrorMessage.setVisibility(View.VISIBLE);
+    }
+
 
     @Override
     protected void onDestroy() {
